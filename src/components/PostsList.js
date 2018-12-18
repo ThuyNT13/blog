8f8edb1591a79bd7a44ead570bb10e539ce1b00d
ThuyNT13 <ngo.thuy38@gmail.com>
2018-12-17 17:34:29 -0800

    Add mapStateToProps to render posts

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -8,11 +8,27 @@ class PostsList extends Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map((post) => {
+      return(
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user"/>
+          <div className="content">
+            <article className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </article>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     console.log(this.props.posts)
     return(
-      <section>
-        PostsLists
+      <section className="ui relaxed divided list">
+        {this.renderList()}
       </section>
     )
   }
